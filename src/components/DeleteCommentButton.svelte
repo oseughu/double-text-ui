84b3a6afb74f1af3766ec57df6f899fc1395148d
Ose Ughu <oseughu@gmail.com>
2022-11-17 07:15:57 +0100

    change delete icon

--- src/components/DeleteCommentButton.svelte
+++ src/components/DeleteCommentButton.svelte
@@ -1,27 +1,30 @@
 <script>
   import { user } from '$/stores'
   import { PUBLIC_API_URL } from '$env/static/public'
-  import MdDelete from 'svelte-icons/md/MdDelete.svelte'
+  import FaTrashAlt from 'svelte-icons/fa/FaTrashAlt.svelte'
 
   export let post
   export let comment
 
   const deleteComment = async () => {
-    const res = await fetch(`${PUBLIC_API_URL}/posts/${post._id}/comments/${comment._id}/delete`, {
-      method: 'DELETE',
-      credentials: 'include'
-    })
-
-    if (res.status === 200) {
-      location.reload()
+    if ($user.name === undefined) {
+      await goto('/login')
     } else {
-      alert('error deleting post, please try again.')
+      await fetch(`${PUBLIC_API_URL}/posts/${post._id}/comments/${comment._id}/delete`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+      })
+
+      location.reload()
     }
   }
 </script>
 
 {#if $user._id === post.author._id || $user._id === comment.author._id}
   <form on:submit|preventDefault={deleteComment}>
-    <button class="btn btn-danger del" type="submit"><MdDelete /></button>
+    <button class="btn btn-danger del" type="submit"><FaTrashAlt /></button>
   </form>
 {/if}
--- src/components/DeletePostButton.svelte
+++ src/components/DeletePostButton.svelte
@@ -1,26 +1,29 @@
 <script>
   import { user } from '$/stores'
   import { PUBLIC_API_URL } from '$env/static/public'
-  import MdDelete from 'svelte-icons/md/MdDelete.svelte'
+  import FaTrashAlt from 'svelte-icons/fa/FaTrashAlt.svelte'
 
   export let post
 
   const deletePost = async () => {
-    const res = await fetch(`${PUBLIC_API_URL}/posts/${post._id}/delete`, {
-      method: 'DELETE',
-      credentials: 'include'
-    })
-
-    if (res.status === 200) {
-      location.reload()
+    if ($user.name === undefined) {
+      await goto('/login')
     } else {
-      alert('error deleting post, please try again.')
+      await fetch(`${PUBLIC_API_URL}/posts/${post._id}/delete`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        credentials: 'include'
+      })
+
+      location.reload()
     }
   }
 </script>
 
 {#if $user._id === post.author._id}
   <form on:submit|preventDefault={deletePost}>
-    <button class="btn btn-danger del" type="submit"><MdDelete /></button>
+    <button class="btn btn-danger del" type="submit"><FaTrashAlt /></button>
   </form>
 {/if}
