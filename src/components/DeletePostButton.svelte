82b161d490957c7c63bd5a002523f7f64ad914dd
Ose Ughu <oseughu@gmail.com>
2022-11-17 08:47:11 +0100

    add inline-block to form

--- src/components/DeletePostButton.svelte
+++ src/components/DeletePostButton.svelte
@@ -23,7 +23,7 @@
 </script>
 
 {#if $user._id === post.author._id}
-  <form on:submit|preventDefault={deletePost}>
+  <form class="del-form" on:submit|preventDefault={deletePost}>
     <button class="btn btn-danger del" type="submit"><MdDelete /></button>
   </form>
 {/if}
