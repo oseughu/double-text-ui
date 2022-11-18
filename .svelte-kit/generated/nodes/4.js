a96bee0691c3791093330513c9c5be47e411236f
Ose Ughu <oseughu@gmail.com>
2022-11-18 15:47:49 +0100

    remove .svelte-kit folder

--- .svelte-kit/generated/nodes/4.js
+++
@@ -1,3 +0,0 @@
-import * as shared from "../../../src/routes/posts/+page.js";
-export { shared };
-export { default as component } from "../../../src/routes/posts/+page.svelte";
--- .svelte-kit/generated/nodes/6.js
+++
@@ -1,3 +0,0 @@
-import * as shared from "../../../src/routes/posts/[id]/comments/[commentId]/replies/new/+page.js";
-export { shared };
-export { default as component } from "../../../src/routes/posts/[id]/comments/[commentId]/replies/new/+page.svelte";
